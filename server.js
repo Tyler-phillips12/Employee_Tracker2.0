951ce189a05fc76ee38e7eb35aec62c15bce8b12
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-05-18 16:40:04 -0400

    mysql2 inquirer and connection to db

---
+++ server.js
@@ -0,0 +1,20 @@
+// import mysql2
+const mysql = require('mysql2')
+// import inquirer 
+const inquirer = require('inquirer'); 
+
+require('dotenv').config()
+
+// connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employee_db'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  afterConnection();
+});
